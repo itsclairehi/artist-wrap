a1a90676716a3551856a90206ac36167c64e7f3a
claire p <cpuckett9@gmail.com>
2021-09-07 15:04:11 -0500

    added alert if fields not filled in

--- assets/sendMail.js
+++ assets/sendMail.js
@@ -1,27 +1,34 @@
 function sendMail(event){
-    var name="Artist Name: "+document.getElementById('aname').value
-    var pname="Partner name: "+document.getElementById('pname').value
-    var cname="Client's name(s): "+document.getElementById('cname').value
-    var sessions="session dates: "+document.getElementById('sessions').value
-    var songTitle=document.getElementById('songTitle').value
-    var description=document.getElementById('description').value
 
-    console.log(name, pname, cname, sessions )
-    console.log("hello")
+  //field input values
+var aNameVal = document.getElementById('aname').value
+var pNameVal= document.getElementById('pname').value
+var cNameVal= document.getElementById('cname').value
+var sessionsVal = document.getElementById('sessions').value
+var songTitleVal= document.getElementById('songTitle').value
+var descriptionVal= document.getElementById('description').value
+
+  //verbatim in email
+    var name="Artist Name: "+aNameVal
+    var pname="Partner name: "+pNameVal
+    var cname="Client's name(s): "+cNameVal
+    var sessions="session dates: "+sessionsVal
+    
     event.preventDefault()
     
-    //open email
-  document.location.href = 'mailto:claire@austinclassicalguitar.org?subject='+songTitle+' wrap-up&body='+name+'%0D%0A'+pname+'%0D%0A'+cname+'%0D%0A'+sessions+'%0D%0A%0D%0A'+'Description: '+description
+
+//if all fields filled in, send email, else say 'please fill in all fields'
+
+if(aNameVal&&pNameVal&&cNameVal&&sessionsVal&&songTitleVal&&descriptionVal){
+  
+  //writes email
+  document.location.href = 'mailto:claire@austinclassicalguitar.org?subject='+songTitleVal+' wrap-up&body='+name+'%0D%0A'+pname+'%0D%0A'+cname+'%0D%0A'+sessions+'%0D%0A%0D%0A'+'Description: '+descriptionVal
 
   window.alert("Please attach mp3 or wav file of song to email. Thank you!!")
-
+  
+} else{
+  window.alert("Please fill out all fields.")
+  
 }
 
-
-{/* <script type="text/javascript">
-  var email = "someone@microsoft.com";
-  document.write('<a href="mailto:'+email+'">email</a>');
-</script> */}
-
-//create email and email button
-//   document.write('<a href="mailto:cpuckett9@gmail.com?subject='+songTitle+' wrap-up&body='+name+'%0D%0A'+pname+'%0D%0A'+cname+'%0D%0A'+sessions+'%0D%0A'+description+'">email</a>')
+}
